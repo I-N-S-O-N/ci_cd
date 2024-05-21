c1f5a3dc62810d338cebe1e20f513c40c789afdc
I-N-S-O-N <79045287+I-N-S-O-N@users.noreply.github.com>
2024-05-21 18:48:47 +0500

    init

---
+++ src/helpers/sum.js
@@ -0,0 +1,15 @@
+export const sum = (a, b) => {
+    return a * b
+};
+
+// const numbers = [1,2,3]
+
+// const value = 'braim'
+// console.log(value);
+
+// function sum() {
+//   for (const iterator of numbers) {
+//     console.log(iterator);
+//   }
+// }
+// sum()
