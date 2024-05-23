dc7ce0e0b44c0b52f5a89ec177f148dd8d9e9614
I-N-S-O-N <79045287+I-N-S-O-N@users.noreply.github.com>
2024-05-23 16:28:57 +0500

    unit testing fail fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">learn react</div>;
 }
 
 export default App;
